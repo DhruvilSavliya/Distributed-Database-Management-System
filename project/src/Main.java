1d72a79b130a424c340fa8bc866c6cf4bee4fc42
Tanuj Sobti <tn308788@dal.ca>
2021-04-02 12:53:34 -0300

    Parser code  updated

--- project/src/Main.java
+++ project/src/Main.java
@@ -6,33 +6,33 @@ public class Main {
 
     public static void main(String [] args)
     {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Welcome to Group 18's DBMS Project. Please select an option");
-        System.out.println("1. Login" +"\n"
-                +"2. Register");
-        Integer option = scanner.nextInt();
-        if (option == 1) {
-            Login login = new Login();
-            if (login.login()) {
-                System.out.println("Login successful"); //Main flow to app goes here
-            } else {
-                System.out.println("Login failed. Please try again.");
-            }
-        }
-        else if (option == 2) {
-            Registration registration = new Registration();
-            if (registration.register()) {
-                System.out.println("User successfully registered!");
-            } else {
-                System.out.println("Registration failed. Please try again.");
-            }
-        }
-
-//        Parser p=new Parser();
-//        try {
-//            p.userInput();
-//        } catch (Exception e) {
-//            e.printStackTrace();
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Welcome to Group 18's DBMS Project. Please select an option");
+//        System.out.println("1. Login" +"\n"
+//                +"2. Register");
+//        Integer option = scanner.nextInt();
+//        if (option == 1) {
+//            Login login = new Login();
+//            if (login.login()) {
+//                System.out.println("Login successful"); //Main flow to app goes here
+//            } else {
+//                System.out.println("Login failed. Please try again.");
+//            }
 //        }
+//        else if (option == 2) {
+//            Registration registration = new Registration();
+//            if (registration.register()) {
+//                System.out.println("User successfully registered!");
+//            } else {
+//                System.out.println("Registration failed. Please try again.");
+//            }
+//        }
+
+        Parser p=new Parser();
+        try {
+            p.userInput();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- project/src/Parser.java
+++ project/src/Parser.java
@@ -7,11 +7,15 @@ public class Parser {
     final String selectRegex = "^SELECT\\s.\\sFROM\\s\\w{1,}\\sWHERE\\s\\w{1,}..\\w{1,}..*";
     final String insertRegex = "^INSERT\\sINTO\\s\\w{1,}\\sVALUES.\\w{1,}.*";
     final String deleteRegex = "^DELETE\\sFROM\\s\\w{1,}\\sWHERE\\s.*";
+    final String createTableRegex = "^CREATE\\sTABLE\\s\\w{1,}\\s.*.";
+
 
     public void userInput() throws Exception {
         final Pattern selectPattern = Pattern.compile(selectRegex);
         final Pattern insertPattern = Pattern.compile(insertRegex);
         final Pattern deletePattern = Pattern.compile(deleteRegex);
+        final Pattern createTablePattern = Pattern.compile(createTableRegex);
+
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your query");
@@ -19,6 +23,8 @@ public class Parser {
         final Matcher selectMatcher = selectPattern.matcher(queryInput);
         final Matcher insertMatcher = insertPattern.matcher(queryInput);
         final Matcher deleteMatcher = deletePattern.matcher(queryInput);
+        final Matcher createTableMatcher = createTablePattern.matcher(queryInput);
+
         FileWriter myFile = new FileWriter("queryParsing.txt");
 
         if(selectMatcher.find())
@@ -39,6 +45,12 @@ public class Parser {
             myFile.flush();
             myFile.close();
             // delete methode code goes here
+        }else if(createTableMatcher.find())
+        {
+            myFile.append("[Create table query]").append(queryInput).append("\n");
+            myFile.flush();
+            myFile.close();
+            // Create table methode code goes here
         }
 
 
